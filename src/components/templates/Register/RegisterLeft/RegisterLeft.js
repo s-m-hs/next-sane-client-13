80b409889b683711d9a19dcb1051d2568d316e99
unknown <smhsseyyed@gmail.comm>
2024-07-31 15:24:38 -0700

    any

--- src/components/templates/Register/RegisterLeft/RegisterLeft.js
+++ src/components/templates/Register/RegisterLeft/RegisterLeft.js
@@ -4,6 +4,8 @@ export default function RegisterLeft() {
   return (
     <div className={style.div}>
       <img className={style.img} src="../../../../../images/register/user-privacy-control-icon-design-vector.jpg" alt="" />
+      {/* <img className={style.img} src="../../../../../images/register/11168381.png" alt="" /> */}
+      {/* <img className={style.img} src="../../../../../images/register/3893155.png" alt="" /> */}
     </div>
   )
 }
