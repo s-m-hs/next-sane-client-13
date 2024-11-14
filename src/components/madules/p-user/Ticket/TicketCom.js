1bcda1bb5b0f85faccd1fad5b43ef30530d04c19
s-m-hs <161473021+s-m-hs@users.noreply.github.com>
2024-11-14 13:46:01 +0330

    ticket

--- src/components/madules/p-user/Ticket/TicketCom.js
+++ src/components/madules/p-user/Ticket/TicketCom.js
@@ -4,10 +4,227 @@ import { useForm } from "react-hook-form";
 import apiUrl from '@/utils/ApiUrl/apiUrl';
 import { MainContext } from '@/context/MainContext';
 import DateFormat from '@/utils/DateFormat';
+import Modal from 'react-bootstrap/Modal';
+import { Toast } from 'primereact/toast';
 
 export default function TicketCom() {
 let{cyUserID,mobile}=useContext(MainContext)
 const [ticketArray,setTicketArray]=useState([])
+
+const [lgShow, setLgShow] = useState(false);
+const [textArea, setTextArea] = useState("");
+const [guIdC, setGuIdC] = useState("");
+const [stateId, setStateID] = useState(1);
+const [ticketId,setTicketId]=useState('')
+const [getChats,setGetChats]=useState([])
+const [userId, setUserId] = useState(cyUserID);
+const [flag, setFlag] = useState(false);
+const [file, setFile] = useState({});
+
+const classRefD = useRef();
+
+const toastB = useRef(null);
+const show = (stat,title) => {
+    toastB.current.show({ severity: stat, summary:stat, detail: title});
+};
+
+const downloadFileB=(gui)=>{
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: " فابل در حال دانلود است",
+    showConfirmButton: false,
+    timer: 500,
+    // color:'#208fe0'
+  }).then(res=>{
+     async function myApp(){
+    const res=fetch(`${apiUrl}/api/CyFiles/download/${gui}`,{
+      method:'GET',
+      headers: {
+        // Authorization: headerAuth,
+        "Content-Type": "application/json",
+      },
+    }).then(res=>{
+      if(res.status==200){
+        window.location.href=res.url
+        //  alert(`${res.url}`)
+      //  navigate(`${res.url}`)
+      //  redirect(`${res.url}`)
+    }
+    })
+  }
+  myApp()
+  });
+
+
+}
+const changeUplode = () => {
+  // event.preventDefault()
+  let formData = new FormData();
+  formData.append("File", file);
+  formData.append("Name", "");
+  formData.append("Description", "");
+  formData.append("IsPrivate", true);
+  // console.log(formData.get('File'));
+  async function myAppPostFile() {
+    const res = await fetch(`${apiUrl}/api/CyFiles/upload`, {
+      method: "POST",
+      // headers: {
+      //   // Authorization: `Bearer ${cmsContext.token.token}`,
+      //   // 'accept': '*/*',
+      //   // 'Content-Type': 'multipart/form-data',
+      //   // // "Content-Type": "application/json",
+
+      // },
+      body: formData,
+    })
+      .then((res) => {
+        if (res.status == 200) {
+          classRefD.current.classList.remove("ticket-hide");
+          // classRefC.current.classList.add('order-show')
+          return res.json();
+        }
+      })
+      .then((result) => {
+        // console.log(result);
+        if (result) {
+          setGuIdC(result.id);
+          classRefD.current.classList.remove("ticket-hide");
+          // classRefC.current.classList.add('order-show')
+        }
+      }).catch(err=>console.log(err))
+  } 
+  myAppPostFile();
+};
+const notify = () => {
+  setFlag((prev) => !prev);
+  setTextArea("");
+  setGuIdC('')
+  setFile('')
+  classRefD.current.classList.add("ticket-hide");
+  if(stateId==1){
+    setGetChats([])
+    setTicketId('')
+    setLgShow(false)
+    setFlagMessageNotification(prev=>!prev)
+  }
+};
+const notifyB = () => {
+  setFlag((prev) => !prev);
+  setTextArea("");
+    setGetChats([])
+    setTicketId('')
+
+};
+const changeTextArea = (e) => {
+  setTextArea(e.target.value);
+};
+const fileChange = (e) => {
+     
+  setFile(e.target.files[0]);
+
+};
+const sendTicket=()=>{
+  let obj={
+      id: 0,
+      senderID: 0,
+      senderName: "string",
+      ticketID: ticketId,
+      description: textArea ? textArea : guIdC? 'ارسال فایل':'' ,
+      status: 1,
+      sentDate: "2024-11-05T08:26:17.313Z",
+      seenDate: "2024-11-05T08:26:17.313Z",
+      fileID: guIdC ? guIdC : null,
+    }
+console.log(obj)
+async function myApp(){
+  const getLocalStorage = localStorage.getItem('loginToken')
+
+  const res=await fetch(`${apiUrl}/api/CyTicket/postOnTicket`,{
+      method:'POST',
+      headers: {
+          Authorization: `Bearer ${getLocalStorage}`,
+          "Content-Type": "application/json",
+        },
+        body:JSON.stringify(obj)
+  }).then(res=>{
+    console.log(res)
+      if(res.status==200){
+          show("info",' پیام با موفقیت ارسال شد')
+          notify(); 
+          classRefD.current.classList.add("ticket-hide");
+          return res.json()
+      }
+  }).then(result=>{
+      setFile({})
+  }).catch(err=>console.log(err))
+}
+myApp()
+}
+
+const getThicketById=()=>{
+  const getLocalStorage = localStorage.getItem('loginToken')
+
+  setGetChats([])
+  async function myApp(){
+      const res=await fetch(`${apiUrl}/api/CyTicket/getMessagesForTickets?TicketID=${ticketId}`,{
+          method:'GET',
+          headers: {
+              Authorization: `Bearer ${getLocalStorage}`,
+              "Content-Type": "application/json",
+            },
+      }).then(res=>{
+          if(res.status==200){
+              return res.json()
+          }
+      }).then(result=>{
+          // console.log(result)
+          setGetChats(result)
+
+      }).catch(err=>console.log(err))
+  }
+  myApp()
+}
+
+const closeChat=()=>{
+  const getLocalStorage = localStorage.getItem('loginToken')
+
+  Swal.fire({
+    title: " آیا از بستن تیکت اطمینان دارید؟",
+    showDenyButton: true,
+    // showCancelButton: true,
+    confirmButtonText: "بله",
+    denyButtonText: `خیر`
+  }).then((result) => {
+    if (result.isConfirmed) {
+      async function myApp(){
+        const res= await fetch(`${apiUrl}/api/CyTicket/closeTicket?ticketId=${ticketId}`,{
+          method:'PUT',
+          headers: {
+            Authorization: `Bearer ${getLocalStorage}`,
+            "Content-Type": "application/json",
+          },
+        }).then(res=>{
+          // show("info"," پیام با موفقیت بسته شد...");
+          notifyB()
+        }).catch(err=>console.log(err))
+      }
+      myApp()
+  //     Swal.fire({
+  //   position: "center",
+  //   icon: "success",
+  //   title: " فابل در حال دانلود است",
+  //   showConfirmButton: false,
+  //   timer: 500,
+  // })
+ } 
+   
+  })
+
+
+
+
+}
 
     const {
         register,
@@ -20,6 +237,13 @@ const [ticketArray,setTicketArray]=useState([])
       });
       const handleError = (errors) => {};
 
+      
+    const stateArraySelect = [
+      { id: 1, state: "درانتظار پاسخ" },
+      { id: 2, state: " پاسخ داده شده" },
+      { id: 3, state: "بسته شده" },
+  
+    ];
     // const[tickettitle,setTickettitle]=useState('')
     // const[ticketFirstDetaile,setTicketFirstDetaile]=useState('')
 
@@ -94,12 +318,21 @@ useEffect(()=>{
   getAllTicket()
 },[])
 
-
+useEffect(()=>{
+  getThicketById()
+},[ticketId,flag])
+useEffect(() => {
+  if (file) {
+    changeUplode();
+  }
+}, [file]);
 
 
   return (
     <div className='container'>
-
+     <div className="card flex justify-content-center">
+            <Toast ref={toastB} />
+        </div>
         <div className='row mt-1 boxSh'>
             <div className='col-lg-1'>
                 <button className='btn btn-primary btn-lg'
@@ -181,8 +414,12 @@ useEffect(()=>{
   <td><DateFormat dateString={`${item.openedAt}`} /></td>
   <td><DateFormat dateString={`${item.closedAt}`} /></td>
 
-  <td><button className='btn btn-info' >
-    {item.status}
+  <td><button className='btn btn-info' onClick={()=>{
+    setStateID(item.status)
+    setTicketId(item.id)
+    setLgShow(true)
+  }} >
+    {stateArraySelect.filter(filter=>filter.id==item.status)[0].state}
     </button> </td>
 </tr>
     )) 
@@ -195,6 +432,159 @@ useEffect(()=>{
 
           </div>
         </div>
+
+        <>
+        <Modal
+        size="lg"
+        show={lgShow}
+        onHide={() => setLgShow(false)}
+        aria-labelledby="example-modal-sizes-title-lg"
+      >
+        <Modal.Header closeButton>
+      
+
+      
+        </Modal.Header>
+        <Modal.Body><div className='Ticket-div boxSh centerc paziresh-code'>
+
+{
+    stateId != 3  && <div className="ticket-right-message-divB">
+              <textarea 
+                name=""
+                id=""
+                placeholder="پیامت را اینجا بنویس..."
+                value={textArea}
+                onChange={changeTextArea}
+              ></textarea>
+              <div className="ticket-right-message-file-div">
+                <input
+                  type="file"
+                  className="ticket-right-message-file-input"
+                  onChange={fileChange}
+                />
+                <div className="order-file-i ticket-hide "
+                 ref={classRefD}
+                 >
+                  <i class="fa-solid fa-file-circle-check fa-2xl fa-beat-fade" style={{ color:'#63E6BE' ,  marginRight:'40px'}}></i>
+                </div>
+              </div>
+
+              <button className={(textArea || guIdC)  ? "btn btn-info" : 'btn btn-info ticket-disable'}  
+              onClick={sendTicket}
+              >
+                ارسال پیام
+              </button>
+            </div>
+
+}
+<hr /> 
+
+<div className='centerr' style={{justifyContent:'flex-start',backgroundColor:'aliceblue',position:'relative',alignItems:'center'}}>
+
+{stateId!=3 && <div
+  className="ticket-right-message-refresh"
+  onClick={() => {
+    show("info","پیام ها به روز رسانی شد...");
+    notify()          
+  }}
+>
+  <i
+    class="fa-solid fa-rotate fa-xl"
+    style={{ color: "green" }}
+  ></i>
+</div>}
+
+
+<span className='ticket-id-span' >شماره تیکت :  {ticketId}</span>
+{stateId!=3 && ticketId && <button className='btn btn-danger ticket-closemessage-button'
+onClick={()=>{
+  closeChat()
+}}
+> بستن تیکت </button>
+}
+
+</div>
+<div className='centerc ticket-chat-div'>
+{getChats?.length!=0 && getChats?.map(item=>(
+        <div className={
+            item.senderID === userId
+            ? "ticket-messeage-desc-div-sender centerr"
+            : "ticket-messeage-desc-div centerr"
+
+          }>
+          
+            <h5
+              className={
+                item.senderID === userId
+                  ? "ticket-messeage-desc-sender"
+                  : "ticket-messeage-desc"
+              }
+            >
+              {item.description}
+              <span className="ticket-messeage-desc-sendername">{item.senderName
+            }</span>
+               <span className="ticket-messeage-desc-date">
+              {/* {DateFormat(item.sentDate)} */}
+              {/* <DateFormat dateString="2024-10-08T14:30:00Z" /> */}
+              <DateFormat dateString={`${item.sentDate}`} />
+            </span>
+            {item.fileID!=null ? <button 
+            className="ticket-downlod-button"
+            onClick={()=>{
+              setGuIdC(item.fileID)
+
+              Swal.fire({
+                title: "فایل دانلود شود؟",
+                showDenyButton: true,
+                // showCancelButton: true,
+                confirmButtonText: "بله",
+                denyButtonText: `خیر`
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  downloadFileB(item.fileID)
+                  Swal.fire({
+                position: "center",
+                icon: "success",
+                title: " فابل در حال دانلود است",
+                showConfirmButton: false,
+                timer: 500,
+                // color:'#208fe0'
+              }) } 
+               
+              })
+              // downloadFileB(item.fileID)
+            }}><i class="fa-solid fa-file-arrow-down fa-bounce fa-2xl" style={{color:'#74C0FC'}}  ></i></button>: ''
+            }
+            </h5>
+        
+        
+
+            {item.senderID !== userId && item.status == 2 && (
+              <span>
+                <i
+                  class="fa-solid fa-check-double"
+                  style={{ color: "#63E6BE" }}
+                ></i>
+              </span>
+            )}
+            {item.senderID !== userId && item.status == 1 && (
+              <span>
+                <i
+                  class="fa-solid fa-check"
+                  style={{ color: "#63E6BE" }}
+                ></i>
+              </span>
+            )}
+          </div>
+))}
+
+</div>
+
+
+
+</div></Modal.Body>
+      </Modal>
+        </>
     </div>
   )
 }
