cb318b69b04c60c84051a3aad66cb8fa4267f390
s-m-hs <smhsseyyed@gmail.com>
2024-11-22 23:34:45 +0330

    ddd

--- src/components/templatess/Home/SwiperG/SwiperG.js
+++ src/components/templatess/Home/SwiperG/SwiperG.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from 'react';
+import style from './SwiperG.module.css'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -7,7 +8,7 @@ import 'swiper/css';
 import 'swiper/css/effect-cards';
 
 // import required modules
-import { EffectCards } from 'swiper/modules';
+import { EffectCards,Autoplay } from 'swiper/modules';
 
 export default function SwiperG() {
   return (
@@ -15,18 +16,18 @@ export default function SwiperG() {
       <Swiper
         effect={'cards'}
         grabCursor={true}
-        modules={[EffectCards]}
+        modules={[EffectCards,Autoplay]}
         className="mySwiperG"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+        autoplay={{
+            delay: 2000,
+            disableOnInteraction: false,
+          }}      >
+        <SwiperSlide className={style.div}></SwiperSlide>
+        <SwiperSlide className={style.div2}></SwiperSlide>
+        <SwiperSlide className={style.div3}></SwiperSlide>
+        <SwiperSlide className={style.div4}></SwiperSlide>
+        <SwiperSlide className={style.div5}></SwiperSlide>
+   
       </Swiper>
     </>
   );
