f87fb4257dccef330ed8e8c3c1de5a7048245113
W-11 <you@example.com>
2025-01-12 16:45:14 +0330

    fix meta

--- src/components/templatess/ProductDetail/ProductDetailLeft/ProductDetailLeft.js
+++ src/components/templatess/ProductDetail/ProductDetailLeft/ProductDetailLeft.js
@@ -44,7 +44,21 @@ export default function ProductDetailLeft({detail}) {
 console.log(detail)
 console.log(offer)
   return (
-    <div className="container " style={{ height: '600px' }}>
+
+    <>
+          {/* <Head>
+        <meta name="product_id" content={product.id} />
+        <meta name="product_name" content={product.name} />
+        <meta property="og:image" content={product.image} />
+        <meta name="product_price" content={product.price} />
+        <meta name="product_old_price" content={product.oldPrice || ''} />
+        <meta
+          name="availability"
+          content={product.inStock ? 'instock' : 'outofstock'}
+        />
+        <meta name="guarantee" content={product.guarantee || ''} />
+      </Head> */}
+       <div className="container " style={{ height: '600px' }}>
       <div className="row mt-1" style={{ height: '600px' }}>
         {detail && 
         <>
@@ -62,10 +76,7 @@ console.log(offer)
  )
 }
 
-              {/* <span>درگاه اتصال: بلوتوث - دانگل - پورت USB</span>
-              <span>محدوده دقت حسگر: 100-36000 نقطه بر اینچ</span>
-              <span>نوع حسگر: ROG AimPoint</span>
-              <span>سرعت رد‌گیری: 650 اینچ بر ثانیه</span> */}
+      
             </div>
             <div className={`${Styles.ProductDetailL_divMiddle} centerc mt-5`} >
 {detail.supply !=0 ? <>
@@ -191,17 +202,12 @@ console.log(offer)
      
           </>}
 
-
-
-
-
               <span className={Styles.ProductDetailL_divMiddle_count} >موجودی محصول: موجود</span>
         </> :
                       <span className={Styles.ProductDetailL_divMiddle_count} >موجودی محصول:ناموجود</span>
 
         }
 
-    
 
 </div>
 
@@ -211,100 +217,15 @@ console.log(offer)
 
 </div> 
 
-
-
-
-
-
-
-
-
-
-
-
-        {/* <div className="col-12">
-        <div className={`${Styles.ProductDetailL_divright} centerc mt-5`}>
-
-
-
-<span className={Styles.ProductDetailL_div__title} >{detail.name}</span>
-<span className={Styles.ProductDetailL_div__description}  >{detail.description}</span>
-<div className={`${Styles.ProductDetailL_detail} centerc mt-5`}>
-{detail.spec?.slice(0,3).map(item=> 
-<span> {`${item.name} : ${item.value}`}</span>
-)
-}
-</div>
-<div className={`${Styles.ProductDetailL_divMiddle} centerc mt-5`} >
-
-  <span className={Styles.ProductDetailL_divMiddle_offprice} >{detail.price?.toLocaleString()} تومان</span>
-  <span className={Styles.ProductDetailL_divMiddle_price}  >{detail.noOffPrice?.toLocaleString()} تومان</span>
-  <span className={Styles.ProductDetailL_divMiddle_count} >موجودی محصول: موجود</span>
-</div>
-<div className={`${Styles.ProductDetailL_left} centerc mt-5`} >
-  <button className="btn btn-primary "
-          onClick={()=>{
-            const getLocalStorage=localStorage.getItem('loginToken')
-            let obj=[{
-              cyProductID:detail.id,
-              quantity: 1
-            }] 
-            xtFlagLogin ?  updateBasket(getLocalStorage,obj,setBasketFlag,AlertA) 
-            :
-  
-               addToCart(detail.id,'1',setCartCounter)
-            
-          }}
-  >افزودن به سبد خرید</button>
-
-</div>
-
-</div>
-        </div> */}
         </>
         }
 
-      
-
       </div>
-      {/* 
-      <div className="row "style={{height:'200px'}}>
-        <div className="col-md-6">
-          <div className={`${Styles.ProductDetailL_divright} centerc mt-5`}>
-   
 
-
-<span className={Styles.ProductDetailL_div__title} >موس بی سیم گیمینگ ایسوس AimPoint EVA-02 Edition</span>
-<span className={Styles.ProductDetailL_div__description}  >ASUS ROG Gladius III Wireless AimPoint EVA-02 Edition Gaming Mouse</span>
-<span className={Styles.ProductDetailL_div__partn}  >پارت نامبر: 90MP03F0-BMUA00</span>
-<div className={`${Styles.ProductDetailL_detail} centerc mt-5`}> 
-  <span>درگاه اتصال: بلوتوث - دانگل - پورت USB</span>
-<span>محدوده دقت حسگر: 100-36000 نقطه بر اینچ</span>
-<span>نوع حسگر: ROG AimPoint</span>
-<span>سرعت رد‌گیری: 650 اینچ بر ثانیه</span>
-</div>
-
-          </div>
-        </div>
-        <div className="col-md-2">
-
-<div className={`${Styles.ProductDetailL_divMiddle} centerc mt-5`} >
-
-  <span className={Styles.ProductDetailL_divMiddle_offprice} >6,950,000 تومان</span>
-  <span className={Styles.ProductDetailL_divMiddle_price}  >7,445,000 تومان</span>
-  <span className={Styles.ProductDetailL_divMiddle_count} >موجودی محصول: موجود</span>
-</div>
-
-        </div>
-        <div className="col-md-4">
-
-          <div className={`${Styles.ProductDetailL_left} centerc mt-5`} >
-<button className="btn btn-primary ">افزودن به سبد خرید</button>
-
-          </div>
-        </div>
-      </div> */}
     </div>
+    </>
+    
+ 
 
 
   );
